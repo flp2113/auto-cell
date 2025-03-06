d6f8b89fbc1b80a6f583acc7083eaa5075c3dd4b
Felipe Guerzoni <161882746+flp2113@users.noreply.github.com>
2025-03-06 01:06:54 -0300

    Adding color to particles

--- source/Particle/Particle.h
+++ source/Particle/Particle.h
@@ -1,10 +1,12 @@
+#include "../defs.h"
+
 enum Particle_Type
 {
-	EMPTY = 0, SAND, ROCK
+	EMPTY = 0, SAND, ROCK, WATER
 };
-
 
 typedef struct Particle
 {
+	Vec4 color;
 	Particle_Type type;
 } Particle;
