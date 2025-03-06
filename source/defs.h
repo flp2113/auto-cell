db7a458ebf1485844be659fd67a40cd1bd971cc2
Felipe Guerzoni <161882746+flp2113@users.noreply.github.com>
2025-03-06 01:07:13 -0300

    Defining vecs.

--- source/defs.h
+++ source/defs.h
@@ -1,5 +1,20 @@
 #pragma once
 
+typedef struct Vec2
+{
+	float x, y;
+} Vec2;
+
+typedef struct Vec3
+{
+	float x, y, w;
+} Vec3;
+
+typedef struct Vec4
+{
+	float x, y, w, h;
+} Vec4;
+
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define MIN_WINDOW_WIDTH 640
